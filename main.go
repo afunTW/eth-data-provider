0d637a750cf653f883193af2629f4809a180eb30
afunTW <afun@afun.tw>
2023-12-31 00:03:46 +0800

    feature: spawn server in goroutine and wait signal

--- main.go
+++ main.go
@@ -1,3 +1,31 @@
-package ethdataprovider
+package main
 
-func main() {}
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/afunTW/eth-data-provider/src/config"
+	"github.com/afunTW/eth-data-provider/src/router"
+	"github.com/afunTW/eth-data-provider/src/service"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	log.Info("Start service")
+	// run service in coroutine
+	config := config.NewConfig()
+	router := router.NewRouter(config)
+	service := service.NewApiService(config, router)
+	go service.Run(ctx)
+
+	// block until recv signal
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Info("End service")
+}
