d6749941a83be8b6415066fe2e091bc9f2d15d0e
afunTW <afun@afun.tw>
2023-12-31 00:03:17 +0800

    feature: add server run

--- src/service/api_service.go
+++ src/service/api_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/afunTW/eth-data-provider/src/config"
@@ -27,4 +28,24 @@ func NewApiService(config *config.Config, router *gin.Engine) (service *ApiServi
 		router: router,
 		server: server,
 	}
+}
+
+func (s *ApiService) Run(ctx context.Context) error {
+	log.Info("Run server")
+	if s.router == nil {
+		log.Fatal("Missing router")
+	}
+
+	// run server
+	// log the unexpected error before it raise
+	err := s.server.ListenAndServe()
+	defer func() {
+		if err != nil && err != http.ErrServerClosed {
+			log.Errorf("Unexpected http server error: %v\n", err)
+		}
+	}()
+	if err != http.ErrServerClosed {
+		return err
+	}
+	return nil
 }
