e916254fae8f9bc38cfad41ecb7bf172ddf86fb1
afunTW <afun@afun.tw>
2023-12-31 17:09:01 +0800

    feature: init get latest block handler and model

--- src/handler/handler_v1_impl.go
+++ src/handler/handler_v1_impl.go
@@ -1,8 +1,12 @@
 package handler
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/afunTW/eth-data-provider/src/config"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
 type handlerV1Impl struct {
@@ -13,6 +17,24 @@ func NewHandlerV1Impl(config *config.Config) Handler {
 	return &handlerV1Impl{config: config}
 }
 
-func (h *handlerV1Impl) GetLatestNBlocks(g *gin.Context)       {}
-func (h *handlerV1Impl) GetBlockById(g *gin.Context)           {}
-func (h *handlerV1Impl) GetTransactionByTxHash(g *gin.Context) {}
+func (h *handlerV1Impl) GetLatestBlocks(ctx *gin.Context) {
+	var query ReqGetLatestBlocks
+	if err := ctx.ShouldBindQuery(&query); err != nil {
+		log.Error(err)
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, RespStatus{
+			Message:   "Fail to parse the query",
+			Timestamp: uint64(time.Now().UnixNano()),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, RespGetLatestBlocks{
+		Blocks: []BlockInfo{},
+		RespStatus: RespStatus{
+			Message:   "Success",
+			Timestamp: uint64(time.Now().UnixNano()),
+		},
+	})
+}
+
+func (h *handlerV1Impl) GetBlockById(ctx *gin.Context)           {}
+func (h *handlerV1Impl) GetTransactionByTxHash(ctx *gin.Context) {}
---
+++ src/handler/model.go
@@ -0,0 +1,34 @@
+package handler
+
+// ==============================
+// Request model
+// ==============================
+
+type ReqGetLatestBlocks struct {
+	Limit uint16 `form:"limit"`
+}
+
+// ==============================
+// Response model
+// ==============================
+
+type RespStatus struct {
+	Message   string `json:"message"`
+	Timestamp uint64 `json:"timestamp"`
+}
+
+type RespGetLatestBlocks struct {
+	Blocks []BlockInfo `json:"blocks"`
+	RespStatus
+}
+
+// ==============================
+// Component model
+// ==============================
+
+type BlockInfo struct {
+	BlockNum   int    `json:"block_num"`
+	BlockHash  string `json:"block_hash"`
+	BlockTime  uint64 `json:"block_time"`
+	ParentHash string `json:"parent_hash"`
+}
