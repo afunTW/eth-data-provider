78db26494f6c68c992b6e6ac3c061999a4383d66
afunTW <afun@afun.tw>
2024-01-07 00:00:27 +0800

    feature: use rpc instead of http to communicate

--- src/repository/repository.go
+++ src/repository/repository.go
@@ -1,7 +1,1 @@
 package repository
-
-type EthereumRepository interface {
-	GetBlockByNumber(blockNumber string) (*EthereumBlock, error)
-	GetTransactionByHash(txHash string) (*EthereumTransaction, error)
-	GetTransactionReceipt(txHash string) (*EthereumTransactionReceipt, error)
-}
